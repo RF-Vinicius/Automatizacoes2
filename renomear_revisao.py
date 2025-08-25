c37cdec6d2095efb36e6d27040026f72ff4c120a
RF-Vinicius <reisf.vinicius@gmail.com>
2025-08-25 11:44:38 -0300

    renomear_revisao

---
+++ renomear_revisao.py
@@ -0,0 +1,62 @@
+import pandas as pd
+import os
+import shutil
+import re
+
+def renomear_revisao_docs(caminho_doc):
+    
+    caminho_pasta_nao_encontrados = os.path.join(caminho_doc, "NaoEncontradosRevisao")
+    os.makedirs(caminho_pasta_nao_encontrados, exist_ok=True)
+
+    padrao01 = r"(R)(\d{2})"
+    padrao02 = r"(V)(\d{2})"
+    padrao03 = r"MN_COMMENTS"
+
+    # Percorrer todos os arquivos na pasta de origem
+    for nome_arquivo in os.listdir(caminho_doc):
+        if os.path.isdir(os.path.join(caminho_doc, nome_arquivo)):
+            continue
+        
+        nome_base, extensao = os.path.splitext(nome_arquivo)
+        listNome = str(nome_base).split("-")
+        
+             
+        if (re.search(padrao01, listNome[-1])) and not (re.search(padrao03, nome_base)):
+            novo_nome = listNome
+            novo_nome[-1] = novo_nome[-1].replace("R","E")   
+            novo_nome_documento = "-".join(novo_nome) + extensao
+            print(novo_nome_documento)
+        
+        elif re.search(padrao02, "".join(listNome[-2:])):
+            novo_nome = listNome
+            novo_nome_documento = "-".join(novo_nome[:-2]) + "-" + "".join(novo_nome[-2:]) + extensao
+            print(novo_nome_documento)     
+        else:
+            # O arquivo não foi encontrado na planilha, movê-lo para a pasta de não encontrados
+            caminho_origem = os.path.join(caminho_doc, nome_arquivo)
+            caminho_destino = os.path.join(caminho_pasta_nao_encontrados, nome_arquivo)
+            
+            try:
+                shutil.move(caminho_origem, caminho_destino)
+                print(f"↔ Movido: '{nome_arquivo}' -> '{caminho_pasta_nao_encontrados}'")
+            except Exception as e:
+                print(f"ERRO ao mover o arquivo '{nome_arquivo}': {e}")
+            continue
+
+        caminho_antigo = os.path.join(caminho_doc, nome_arquivo)
+        caminho_novo = os.path.join(caminho_doc, novo_nome_documento)
+
+        try:
+            os.rename(caminho_antigo, caminho_novo)
+            print(f"Renomeado: '{nome_arquivo}' -> '{novo_nome_documento}'")
+        except FileExistsError:
+            print(f"ALERTA: O arquivo '{novo_nome_documento}' já existe. Não foi possível renomear '{nome_arquivo}'.")
+        except Exception as e:
+            print(f"ERRO ao renomear o arquivo '{nome_arquivo}': {e}")
+
+if __name__ == "__main__":
+
+    caminho_doc = r"C:\Users\vini9\OneDrive\Área de Trabalho\ConstruCode\Automatizacoes2\teste"
+
+    renomear_revisao_docs(caminho_doc)
+        
