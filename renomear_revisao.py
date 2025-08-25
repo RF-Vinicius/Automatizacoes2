02d787c3ddd1652b4c898a3df7cbff6bcd3e9dde
RF-Vinicius <reisf.vinicius@gmail.com>
2025-08-25 13:31:00 -0300

    fix renomear_revisao

--- renomear_revisao.py
+++ renomear_revisao.py
@@ -10,7 +10,8 @@ def renomear_revisao_docs(caminho_doc):
 
     padrao01 = r"(R)(\d{2})"
     padrao02 = r"(V)(\d{2})"
-    padrao03 = r"MN_COMMENTS"
+    padrao02_1 = r"(VR)(\d{2})"
+    padrao03 = r"MN-COMMENTS" #Ajustei aqui para - por conta do replace
 
     # Percorrer todos os arquivos na pasta de origem
     for nome_arquivo in os.listdir(caminho_doc):
@@ -18,19 +19,24 @@ def renomear_revisao_docs(caminho_doc):
             continue
         
         nome_base, extensao = os.path.splitext(nome_arquivo)
+        nome_base = str(nome_base).replace("_", "-")
         listNome = str(nome_base).split("-")
         
-             
-        if (re.search(padrao01, listNome[-1])) and not (re.search(padrao03, nome_base)):
+        
+        if (re.search(padrao02, "".join(listNome[-2:]))) or (re.search(padrao02_1, "".join(listNome[-2:]))) : #Caso 02
+            novo_nome = listNome
+            novo_nome_documento = "-".join(novo_nome[:-2]) + "-" + "".join(novo_nome[-2:]).replace("R", "") + extensao
+            print(novo_nome_documento)     
+        elif (re.search(padrao03, nome_base)): #Caso 03
+            novo_nome = nome_base.replace("-MN-COMMENTS", "").split('-')
+            novo_nome.pop(8)
+            novo_nome_documento = "-".join(novo_nome[:-2]) + "-" + "".join(novo_nome[-2:]).replace("VR", "X") + extensao
+            print (novo_nome_documento)
+        elif (re.search(padrao01, listNome[-1])) and not (re.search(padrao03, nome_base)): #Caso 01
             novo_nome = listNome
             novo_nome[-1] = novo_nome[-1].replace("R","E")   
             novo_nome_documento = "-".join(novo_nome) + extensao
             print(novo_nome_documento)
-        
-        elif re.search(padrao02, "".join(listNome[-2:])):
-            novo_nome = listNome
-            novo_nome_documento = "-".join(novo_nome[:-2]) + "-" + "".join(novo_nome[-2:]) + extensao
-            print(novo_nome_documento)     
         else:
             # O arquivo não foi encontrado na planilha, movê-lo para a pasta de não encontrados
             caminho_origem = os.path.join(caminho_doc, nome_arquivo)
