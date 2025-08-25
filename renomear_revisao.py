1b289f15a7616381cf1d1693192088014379a920
RF-Vinicius <reisf.vinicius@gmail.com>
2025-08-25 13:47:40 -0300

    fix 2 renomear arquivo

--- renomear_revisao.py
+++ renomear_revisao.py
@@ -11,7 +11,7 @@ def renomear_revisao_docs(caminho_doc):
     padrao01 = r"(R)(\d{2})"
     padrao02 = r"(V)(\d{2})"
     padrao02_1 = r"(VR)(\d{2})"
-    padrao03 = r"MN-COMMENTS" #Ajustei aqui para - por conta do replace
+    padrao03 = r"MN_COMMENTS" #Ajustei aqui para - por conta do replace
 
     # Percorrer todos os arquivos na pasta de origem
     for nome_arquivo in os.listdir(caminho_doc):
@@ -19,24 +19,26 @@ def renomear_revisao_docs(caminho_doc):
             continue
         
         nome_base, extensao = os.path.splitext(nome_arquivo)
-        nome_base = str(nome_base).replace("_", "-")
+        #nome_base = str(nome_base).replace("_", "-")
         listNome = str(nome_base).split("-")
         
         
         if (re.search(padrao02, "".join(listNome[-2:]))) or (re.search(padrao02_1, "".join(listNome[-2:]))) : #Caso 02
             novo_nome = listNome
+            novo_nome[-2] = novo_nome[-2].replace("_", "-")
             novo_nome_documento = "-".join(novo_nome[:-2]) + "-" + "".join(novo_nome[-2:]).replace("R", "") + extensao
-            print(novo_nome_documento)     
+            #print(novo_nome_documento)     
         elif (re.search(padrao03, nome_base)): #Caso 03
-            novo_nome = nome_base.replace("-MN-COMMENTS", "").split('-')
+            novo_nome = nome_base.replace("_MN_COMMENTS", "")
+            novo_nome = novo_nome.replace("_", "-").split('-')
             novo_nome.pop(8)
             novo_nome_documento = "-".join(novo_nome[:-2]) + "-" + "".join(novo_nome[-2:]).replace("VR", "X") + extensao
-            print (novo_nome_documento)
+           #print (novo_nome_documento)
         elif (re.search(padrao01, listNome[-1])) and not (re.search(padrao03, nome_base)): #Caso 01
             novo_nome = listNome
             novo_nome[-1] = novo_nome[-1].replace("R","E")   
             novo_nome_documento = "-".join(novo_nome) + extensao
-            print(novo_nome_documento)
+            #print(novo_nome_documento)
         else:
             # O arquivo não foi encontrado na planilha, movê-lo para a pasta de não encontrados
             caminho_origem = os.path.join(caminho_doc, nome_arquivo)
@@ -54,7 +56,7 @@ def renomear_revisao_docs(caminho_doc):
 
         try:
             os.rename(caminho_antigo, caminho_novo)
-            print(f"Renomeado: '{nome_arquivo}' -> '{novo_nome_documento}'")
+            print(f"'{nome_arquivo}' -> '{novo_nome_documento}'")
         except FileExistsError:
             print(f"ALERTA: O arquivo '{novo_nome_documento}' já existe. Não foi possível renomear '{nome_arquivo}'.")
         except Exception as e:
